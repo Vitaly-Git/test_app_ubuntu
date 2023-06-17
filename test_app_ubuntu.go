a5275350c5e501ad1546b3de3a58850496b0ec20
Vitaly <vd.lvov@gmail.com>
2023-06-17 17:06:24 +0300

    1-st commit

---
+++ test_app_ubuntu.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+)
+
+// var serviceAddress = "95.163.241.176:8000"
+// var serviceAddress = "95.163.233.63:8000"
+// var serviceAddress = "51.250.67.25:8000"
+var chEndService = make(chan string)
+
+func main() {
+	fmt.Println("Starting service...")
+	go start_http_server()
+	waiting_for_gorutines_stops()
+}
+
+func start_http_server() {
+
+	http.HandleFunc("/", root_handler)
+	//http.ListenAndServe("localhost:8000", nil)
+
+	// ip, err := get_local_ip()
+	// fmt.Println(ip, err)
+
+	serviceIpAddr, err := GetOutboundIP()
+
+	if err != nil {
+		errorStr := fmt.Sprintf("IP address recognition error: %s", err)
+		chEndService <- errorStr
+		return
+	}
+
+	servicePortAddr := "8000"
+	serviceAddress := fmt.Sprintf("%s:%s", serviceIpAddr, servicePortAddr)
+
+	http.ListenAndServe(serviceAddress, nil)
+}
+
+func waiting_for_gorutines_stops() {
+	<-chEndService
+}
+
+func root_handler(resp_writer http.ResponseWriter, r *http.Request) {
+
+	var answer string = ""
+	var url_path string = r.URL.Path
+
+	answer += fmt.Sprintf("Client: %s request: %s\n", r.RemoteAddr, url_path)
+
+	is_service_shuting_down := url_path == "/exit"
+
+	if is_service_shuting_down {
+		answer += fmt.Sprintf("%s\n", "service going to shutting down")
+	}
+
+	fmt.Printf("Answer: %s\n", answer)
+	fmt.Fprintf(resp_writer, answer)
+
+	if is_service_shuting_down {
+		chEndService <- "Service shutdown"
+	}
+
+}
+
+func get_local_ip() (net.IP, error) {
+
+	var ip net.IP
+	var err error
+
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, i := range ifaces {
+
+		var addrs []net.Addr
+
+		addrs, err = i.Addrs()
+		if err != nil {
+			return nil, err
+		}
+
+		for _, addr := range addrs {
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			default:
+				continue
+			}
+			//fmt.Println(ip)
+
+			return ip, nil
+		}
+	}
+
+	return ip, nil
+}
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() (net.IP, error) {
+
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return nil, nil
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP, nil
+}
